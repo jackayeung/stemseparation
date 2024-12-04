837733b506acb7ffe17281f6db81b1531576108e
Jack Yeung <jackyeung@utexas.edu>
2024-12-04 03:18:52 -0600

    working barebones

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -3,6 +3,10 @@ Test script for preprocess.py
 """
 
 import unittest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
 from scripts.preprocess import preprocess_audio
 
 class TestPreprocess(unittest.TestCase):
@@ -10,7 +14,7 @@ class TestPreprocess(unittest.TestCase):
         """
         Test the preprocess_audio function.
         """
-        output_file = preprocess_audio("data/example.mp3")
+        output_file = preprocess_audio("data/Clarity.mp3")
         self.assertTrue(output_file.endswith(".wav"), "Output file should be a .wav file.")
 
 if __name__ == "__main__":
